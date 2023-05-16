507bcb6eb89ae5d747df6131e9c418b9db96a289
nhcminh <a2mrken@gmail.com>
2023-05-16 19:30:58 +0700

    update search todo for return an internal server error

--- backend/src/lambda/http/searchTodos.ts
+++ backend/src/lambda/http/searchTodos.ts
@@ -7,17 +7,31 @@ import { cors, httpErrorHandler } from 'middy/middlewares'
 import { searchTodo } from '../../layers/businessLogic/todos'
 import { getUserId } from '../utils'
 
+import { createLogger } from "../../utils/logger";
+
+const logger = createLogger('searchTodo');
+
 export const handler = middy(
     async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-        const searchValue: string = JSON.parse(event.body ? event.body : "")?.searchValue
-        // TODO: search todos item by string value
-        const userId = getUserId(event)
-        const items = await searchTodo(userId, searchValue)
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                items
-            })
+        try {
+            logger.info('search todo handler')
+            const searchValue: string = JSON.parse(event.body ? event.body : "")?.searchValue
+            // TODO: search todos item by string value
+            const userId = getUserId(event)
+            const items = await searchTodo(userId, searchValue)
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    items
+                })
+            }
+        }
+        catch(e) {
+            logger.info('search todo failed')
+            return {
+                statusCode: 500,
+                body: 'Internal Server error'
+            }
         }
     }
 )
